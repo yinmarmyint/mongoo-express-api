ab1810036d1af14b2bf5cbf98758ea84d1b1ef84
Yin Mar Myint <yinmarmyint@achromex.com>
2019-11-05 11:53:25 +0630

    cors added

--- src/books/index.js
+++ src/books/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 require("./controller/book_controller");
 
 const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.post("/book", (req, res) => {
+  console.log("===>", req.body);
+});
 
 app.listen("8080", () => {
   console.log("Book service is running");
